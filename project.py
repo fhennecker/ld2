5365bb17d2e005c46539fe44ee76ce16f1fefd3f
Florentin Hennecker <florentin.hennecker@gmail.com>
2016-11-22 23:00:28 +0100

    [add] von neumann neighbourhood and snowdrift game

--- project.py
+++ project.py
@@ -1,42 +1,88 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.signal import convolve2d
+from random import shuffle, choice, random
 
-def m(v):
+def moore(v):
     """Moore kernel"""
     return np.array([[v]*3, [v, 0, v], [v]*3])
 
-def payoffs(grid):
-    defecter_payoff = convolve2d(grid, m(10), mode='same', boundary='wrap') * (1 - grid) 
-    cooperators_payoff = convolve2d(grid, m(7), mode='same', boundary='wrap') * grid
+def vonnneumann(v):
+    """Moore kernel"""
+    return np.array([[0,v,0], [v,0,v], [0,v,0]])
+
+def payoffs(grid, method='moore'):
+    f = moore
+    if method == 'vn':
+        f = vonnneumann
+    defecter_payoff = convolve2d(grid, f(10), mode='same', boundary='wrap') * (1 - grid) 
+    cooperators_payoff = convolve2d(grid, f(7), mode='same', boundary='wrap') * grid
     return defecter_payoff + cooperators_payoff
 
+def snowdrift_payoffs(grid, method='moore'):
+    f = moore
+    if method == 'vn':
+        f = vonnneumann
+    defecter_payoff = convolve2d(grid, f(10), mode='same', boundary='wrap') * (1 - grid) 
+    cooperators_payoff = convolve2d(grid, f(7), mode='same', boundary='wrap') * grid
+    cooperators_payoff += convolve2d((1-grid), f(3), mode='same', boundary='wrap') * grid
+    return defecter_payoff + cooperators_payoff
 
-def new_state(grid, payoffs):
+def new_state(grid, payoffs, method='moore'):
     new_grid = np.zeros_like(grid)
     size = len(grid)
     for row in range(size):
         for col in range(size):
             max_index = None
             max_value = -1
-            for dr in [-1, 0, 1]:
-                for dc in [-1, 0, 1]:
+            if method == 'moore':
+                for dr in [-1, 0, 1]:
+                    for dc in [-1, 0, 1]:
+                        index = ((row+dr)%size, (col+dc)%size)
+                        value = payoffs[index] 
+                        if max_index is None or value > max_value:
+                            max_index, max_value = index, value
+            elif method == 'vn':
+                choices = [[-1, 0], [1, 0], [0, -1], [0, 1], [0,0]]
+                shuffle(choices)
+                for dr, dc in choices:
                     index = ((row+dr)%size, (col+dc)%size)
-                    value = payoffs[index] 
+                    value = payoffs[index]
                     if max_index is None or value > max_value:
                         max_index, max_value = index, value
             new_grid[row, col] = grid[max_index]
     return new_grid
                         
-def plot_n_runs(n_simulations, n_steps, size):
+def snowdrift_new_state(grid, payoffs, method='moore'):
+    new_grid = np.zeros_like(grid)
+    size = len(grid)
+    neighbours = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    if method == 'moore':
+        neighbours += [[-1, 1], [-1, -1], [1, -1], [1, 1]]
+    for row in range(size):
+        for col in range(size):
+            c = choice(neighbours)
+            neighbour = ((row+c[0])%size, (col+c[1])%size)
+            N = 8 if method == 'moore' else 4
+            p = 1.*(1 + (payoffs[neighbour]-payoffs[row, col])/(N*10))/2
+            if random() <= p:
+                new_grid[row, col] = grid[neighbour]
+            else:
+                new_grid[row, col] = grid[row, col]
+    return new_grid
+
+def plot_n_runs(n_simulations, n_steps, size, method, game):
     coop = np.zeros((n_simulations, n_steps))
 
     for s in range(n_simulations):
-        print s,
+        print s
         grid = np.random.randint(2, size=(size,size))
         for i in range(n_steps):
             coop[s,i] += grid.mean()
-            grid = new_state(grid, payoffs(grid))
+            if game == 'prisoner':
+                grid = new_state(grid, payoffs(grid, method), method)
+            elif game == 'snowdrift':
+                grid = snowdrift_new_state(grid, snowdrift_payoffs(grid, method), method)
 
     plt.figure(1, figsize=(10,4))
     axlines = plt.axes([0.06,0.15, 0.58, 0.85])
@@ -58,14 +104,19 @@ def plot_n_runs(n_simulations, n_steps, size):
     axhist.hist(coop[:,-1], orientation='horizontal', bins=np.arange(0,1.1,0.1))
     plt.show()
 
-def show_at_steps(size, steps):
+def show_at_steps(size, steps, method, game):
     grid = np.random.randint(2, size=(size, size))
     for i in range(max(steps)+1):
         if i in steps:
             plt.imshow(grid, interpolation='NEAREST', cmap='cool')
-            plt.show()
-        grid = new_state(grid, payoffs(grid))
+            #plt.show()
+            plt.waitforbuttonpress()
+        if game == 'prisoner':
+            grid = new_state(grid, payoffs(grid, method), method)
+        elif game == 'snowdrift':
+            grid = snowdrift_new_state(grid, snowdrift_payoffs(grid, method), method)
+
 
 if __name__ == '__main__':
-    # plot_n_runs(100, 60, 50)
-    show_at_steps(12, [0, 1, 5, 10, 20, 50])
+    plot_n_runs(100, 60, 12, "moore", 'prisoner')
+    #  show_at_steps(50, range(20), 'moore', 'snowdrift')
